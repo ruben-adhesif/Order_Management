96026d230ed5ec19091a7e03a5f30c18b4f74fb7
Ruben-adhesif <rubencapp@gmail.com>
2023-12-28 17:40:42 +0100

    rename user

--- init.bat
+++ init.bat
@@ -1,17 +1,17 @@
 @echo off
 
 echo ~~~~~~~~~~~~~~~~~~ Table Creation ~~~~~~~~~~~~~~~~~~
-sqlplus ruben/root@xepdb1 @table_drop
-sqlplus ruben/root@xepdb1 @table_creation
+sqlplus root/root@xepdb1 @table_drop
+sqlplus root/root@xepdb1 @table_creation
 
 echo ~~~~~~~~~~~~~~~~~~ sqlldr vendor ~~~~~~~~~~~~~~~~~~
-sqlldr ruben/root@xepdb1 control=sqlldr/vendor.ctl log=sqlldr/log/vendor.log
+sqlldr root/root@xepdb1 control=sqlldr/vendor.ctl log=sqlldr/log/vendor.log
 echo ~~~~~~~~~~~~~~~~~~ sqlldr product ~~~~~~~~~~~~~~~~~~
-sqlldr ruben/root@xepdb1 control=sqlldr/product.ctl log=sqlldr/log/product.log
+sqlldr root/root@xepdb1 control=sqlldr/product.ctl log=sqlldr/log/product.log
 echo ~~~~~~~~~~~~~~~~~~ sqlldr header ~~~~~~~~~~~~~~~~~~
-sqlldr ruben/root@xepdb1 control=sqlldr/header.ctl log=sqlldr/log/header.log
+sqlldr root/root@xepdb1 control=sqlldr/header.ctl log=sqlldr/log/header.log
 echo ~~~~~~~~~~~~~~~~~~ sqlldr detail ~~~~~~~~~~~~~~~~~~
-sqlldr ruben/root@xepdb1 control=sqlldr/detail.ctl log=sqlldr/log/detail.log
+sqlldr root/root@xepdb1 control=sqlldr/detail.ctl log=sqlldr/log/detail.log
 
 ::echo ~~~~~~~~~~~~~~~~~~ Connection ~~~~~~~~~~~~~~~~~~
-::sqlplus ruben/root@xepdb1
+::sqlplus root/root@xepdb1
--- role_creation.sql
+++ role_creation.sql
@@ -1,13 +1,13 @@
 -- Creat a role to use SQL*Loader
-CREATE USER ruben IDENTIFIED BY root;
-GRANT DBA TO ruben;
+CREATE USER root IDENTIFIED BY root;
+GRANT DBA TO root;
 
 -- Compulsory grant to use SQL*Loader
-GRANT EXECUTE ON DBMS_SCHEDULER TO ruben;   -- execute external programs
-GRANT EXECUTE ON UTL_FILE TO ruben;		    -- read/write files on database server
-GRANT CREATE DIRECTORY TO ruben;		    -- create a directory object to specifies the location of input/output files
+GRANT EXECUTE ON DBMS_SCHEDULER TO root;   -- execute external programs
+GRANT EXECUTE ON UTL_FILE TO root;		    -- read/write files on database server
+GRANT CREATE DIRECTORY TO root;		    -- create a directory object to specifies the location of input/output files
 
 -- Check the role right
-SELECT * FROM DBA_ROLE_PRIVS WHERE GRANTEE = 'RUBEN';
-SELECT * FROM DBA_SYS_PRIVS WHERE GRANTEE = 'RUBEN';
-SELECT * FROM DBA_TAB_PRIVS WHERE GRANTEE = 'RUBEN';
+SELECT * FROM DBA_ROLE_PRIVS WHERE GRANTEE = 'ROOT';
+SELECT * FROM DBA_SYS_PRIVS WHERE GRANTEE = 'ROOT';
+SELECT * FROM DBA_TAB_PRIVS WHERE GRANTEE = 'ROOT';
